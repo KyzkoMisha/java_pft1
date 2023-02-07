dd4fac72a147eb2056a9893540bc2d791daca789
KyzkoMisha <mishagalkin98@gmail.com>
2023-02-07 21:12:34 +0500

    добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogram.java
@@ -2,6 +2,24 @@ package ru.stqa.pft.sandbox;
 
 public class myfirstprogram {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        hello("world");
+        hello("гандоны");
+        double l =5;
+        System.out.println("площадь квадрата "+l+"="+area(l));
+        double a=4;
+        double b=6;
+        System.out.println("площадь прямоугольник"+a+"и"+b+"="+area(a,b));
+
     }
+    public static void hello(String somebody) {
+        System.out.println("hello "+ somebody);
+
+    }
+    public static double area(double l){
+        return l*l;
+    }
+    public static double area( double a, double b){
+        return a*b;
+    }
+
 }
